560de4f928e0a30879c88a947009dfb51ad95a7f
Adi Linden <adi@adis.ca>
2019-02-13 06:19:13 +0000

    Added simplified ways to test YouTube and Twitch streaming. Added ffmpeg-loop and provided fallthru for running any executable within container.

--- README.md
+++ README.md
@@ -28,8 +28,6 @@ Or, get it from Docker Hub
 
 ## Usage
 
-Run with `shell` command or without command to get dropped into `bash` shell.
-
 Run with `octopi-youtubelive` command and a number of arguments to stream the [OctoPi](https://octoprint.org/) webcam to [YouTube Live](https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig). The following arguments are expected:
 
 - OctoPi Webcam Stream (Example: http://192.168.1.34:8080/?action=stream)
@@ -44,3 +42,50 @@ Example
         rtmp://a.rtmp.youtube.com/live2 \
         xxxx-xxxx-xxxx-xxxx \
         null
+
+Run with `octopi-youtube` command for a simplified way to stream the [OctoPi](https://octoprint.org/) webcam to [YouTube Live](https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig). The following arguments are expected:
+
+- OctoPi Webcam Stream (Example: http://192.168.1.34:8080/?action=stream)
+- YouTube Live Stream Name/Key (Example: xxxx-xxxx-xxxx-xxxx)
+
+Example
+
+    docker run -it --rm --privileged adilinden/rpi-stream:latest octopi-youtube \
+        http://192.168.1.34:8080/?action=stream \
+        xxxx-xxxx-xxxx-xxxx
+
+Run with `octopi-twitch` command for a simplified way to stream the [OctoPi](https://octoprint.org/) webcam to [Twitch.Tv](https://www.twitch.tv/). The following arguments are expected:
+
+- OctoPi Webcam Stream (Example: http://192.168.1.34:8080/?action=stream)
+- Twitch Stream Key (Example: live_123456789_mysupersecrettwitchkey)
+
+Example
+
+    docker run -it --rm --privileged adilinden/rpi-stream:latest octopi-twitch \
+        http://192.168.1.34:8080/?action=stream \
+        live_123456789_mysupersecrettwitchkey
+
+Run with `ffmpeg-loop` and appropriate `ffmpeg` command line parameters to stream the [OctoPi](https://octoprint.org/) webcam to some streaming service of your choice. This places `ffmpeg` inside a while loop and continues to try restarting `ffmpeg` if it fails for any reason, such as connectivity issues. Use with caution! The use of CTRL-C breaks out of the `while` loop if executed interactively.
+
+Example
+
+    docker run -it --rm --privileged adilinden/rpi-stream:latest ffmpeg-loop \
+        -re -f mjpeg -framerate 5 -i http://192.168.1.34:8080/?action=stream \
+        -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
+        -vcodec h264 -pix_fmt yuv420p -framerate 5 -g 10 -strict experimental -filter:v null \
+        -acodec aac -ab 128k -f flv rtmp://live.twitch.tv/app/live_123456789_mysupersecrettwitchkey
+
+Run any supported command within the container. This allows for executing `ffmpeg` with appropriate command line parameters. It is also possible to drop into a shell inside the container.
+
+Examples
+
+    docker run -it --rm --privileged adilinden/rpi-stream:latest ffmpeg \
+        -re -f mjpeg -framerate 5 -i http://192.168.1.34:8080/?action=stream \
+        -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
+        -vcodec h264 -pix_fmt yuv420p -framerate 5 -g 10 -strict experimental -filter:v null \
+        -acodec aac -ab 128k -f flv rtmp://live.twitch.tv/app/live_123456789_mysupersecrettwitchkey
+
+    docker run -it --rm --privileged adilinden/rpi-stream:latest bash
+
+Enjoy!
+
--- entry.sh
+++ entry.sh
@@ -1,34 +1,86 @@
 #!/bin/bash
 
+# Allow CTR-C to exit while loop
+exit_func() {
+        echo "SIGTERM detected"            
+        exit 1
+}
+trap exit_func SIGTERM SIGINT
+
+# Wrapper for ffmpeg
+function run_ffmpeg {
+    local cam_url="$1"
+    local cam_fps="$2"
+    local flv_url="$3"
+    local flv_key="$4"
+    local filter="$5"
+
+    # Define streaming parameters
+    VIDEO_IN="-re -f mjpeg -framerate 5 -i $cam_url"
+    AUDIO_IN="-ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero"
+    VIDEO_OUT="-vcodec h264 -pix_fmt yuv420p -framerate $cam_fps -g $(($cam_fps * 2)) -strict experimental -filter:v $filter"
+    AUDIO_OUT="-acodec aac -ab 128k"
+    STREAM_URL="-f flv $flv_url/$flv_key"
+
+    echo "ffmpeg $VIDEO_IN $AUDIO_IN $VIDEO_OUT $AUDIO_OUT $STREAM_URL"
+
+    ffmpeg $VIDEO_IN $AUDIO_IN $VIDEO_OUT $AUDIO_OUT $STREAM_URL
+}
+
+# See how we were called
 case $1 in
     octopi-youtubelive)
         # Streaming from OctoPi webcam to YouTube Live
+        # Provided for adilinden-oss/octoprint-youtubelive plugin
         echo "Streaming OctoPi webcam to YouTube Live..."
         echo "OctoPi Webcam Stream:         $2"
         echo "YouTube Live Server URL:      $3"
         echo "YouTube Live Stream Name/Key: $4"
         echo "OctoPi Filter:                $5"
 
-        # Define streaming parameters
-        FPS=5
-        VIDEO_IN="-re -f mjpeg -framerate 5 -i $2"
-        AUDIO_IN="-ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero"
-        VIDEO_OUT="-vcodec h264 -pix_fmt yuv420p -framerate $FPS -g $(($FPS * 2)) -strict experimental -filter:v $5"
-        AUDIO_OUT="-acodec aac -ab 128k"
-        STREAM_URL="-f flv $3/$4"
-
-        # Start Stream
-        ffmpeg $VIDEO_IN $AUDIO_IN $VIDEO_OUT $AUDIO_OUT $STREAM_URL
+        run_ffmpeg "$2" "5" "$3" "$4" "$5"
 
         echo "Done streaming..."
         ;;
-    shell)
-        # Executing shell
-        echo "Providing shell..."
-        /bin/bash
+
+    octopi-youtube)
+        # Simplified OctoPi to Twitch.TV
+        echo "Streaming to Twitch.TV..."
+        echo "OctoPi Webcam Stream:         $2"
+        echo "YouTube Live Stream Name/Key: $3"
+
+        run_ffmpeg "$2" "5" "rtmp://a.rtmp.youtube.com/live2" "$3" "null"
         ;;
+
+    octopi-twitch)
+        # Simplified OctoPi to YouTube Live
+        echo "Streaming to YouTube Live..."
+        echo "OctoPi Webcam Stream:         $2"
+        echo "Twitch.Tv Stream Key:         $3"
+
+        run_ffmpeg "$2" "5" "rtmp://live.twitch.tv/app" "$3" "null"
+        ;;
+
+    ffmpeg-loop)
+        # Keep running ffmpeg until explicitely stopped
+        shift
+        while "true"; do
+            echo "Executing \"ffmpeg $(echo $@ | cut -c -55)...\""
+            ffmpeg $@
+
+            # Pause for a bit before retrying
+            echo -n "Pausing... "
+            for ((wait=20; wait>=1; wait--))
+            do
+                echo -n "$wait "
+                sleep 1
+            done
+            echo
+        done
+        ;;
+
     *)
-        # Fallthough dropping into shell
-        echo "Command unknown, providing shell..."
-        /bin/bash
+        # Execute command provided on command line
+        echo "Executing \"$(echo $@ | cut -c -55)...\""
+        $@
 esac
